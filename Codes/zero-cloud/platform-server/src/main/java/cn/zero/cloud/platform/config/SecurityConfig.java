bb09b82d9585a659ca2ad1ccf3803c85c2137702
ACatSmiling <1172042509@qq.com>
2024-06-14 20:11:19 +0800

    security

---
+++ Codes/zero-cloud/platform-server/src/main/java/cn/zero/cloud/platform/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package cn.zero.cloud.platform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * @author Xisun Wang
+ * @since 6/7/2024 15:03
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
