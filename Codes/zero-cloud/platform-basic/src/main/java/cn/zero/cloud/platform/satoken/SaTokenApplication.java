373c75dd82b8ab92d9c635830dc0b8947bc4cc11
ACatSmiling <1172042509@qq.com>
2024-06-18 20:55:28 +0800

    sa token

---
+++ Codes/zero-cloud/platform-basic/src/main/java/cn/zero/cloud/platform/satoken/SaTokenApplication.java
@@ -0,0 +1,15 @@
+package cn.zero.cloud.platform.satoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xisun Wang
+ * @since 6/18/2024 14:19
+ */
+@SpringBootApplication
+public class SaTokenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenApplication.class, args);
+    }
+}
